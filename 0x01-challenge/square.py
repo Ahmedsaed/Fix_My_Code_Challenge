597320511fe7bb2d67fbf816267e16138cc40b59
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-17 02:04:30 +0300

    Fix code style and add documentation in square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Square class """
 
 
-class square():
+class Square():
     """ Square class """
     width = 0
     height = 0
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
     """ Create a square object """
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
