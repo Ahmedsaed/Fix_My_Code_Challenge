53cde0727235bfa6c3cc5b7474687f7ba0b5086b
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-17 01:59:40 +0300

    Fix code style and add documentation in square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" Square class """
+
 
 class square():
-
+    """ Square class """
     width = 0
     height = 0
 
-
     def __init__(self, *args, **kwargs):
+        """ Initialization of the square class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +17,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
